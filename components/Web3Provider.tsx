b4c22a8becf70e9fc3026496e7f978a32a695314
Maksim <mpiatlitski@edu.cdv.pl>
2026-01-09 21:55:33 +0100

    Next

---
+++ components/Web3Provider.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import { defineChain } from 'viem';
+
+const somniaTestnet = defineChain({
+  id: 50312,
+  name: 'Somnia Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Somnia Testnet Token',
+    symbol: 'STT',
+  },
+  rpcUrls: {
+    default: { http: ['https://dream-rpc.somnia.network'] },
+  },
+  blockExplorers: {
+    default: { name: 'Somnia Explorer', url: 'https://somnia-testnet.socialscan.io' },
+  },
+  testnet: true,
+});
+
+const config = getDefaultConfig({
+  appName: 'Somnia Mafia',
+  projectId: 'YOUR_PROJECT_ID', // TODO: Get a project ID or use a placeholder
+  chains: [somniaTestnet],
+  ssr: false, // Vite is client-side
+});
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme()}>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
