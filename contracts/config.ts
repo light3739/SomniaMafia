a8712042a675f1f2a1dc466de3a9c44e840972b3
Maksim <mpiatlitski@edu.cdv.pl>
2026-01-12 02:24:13 +0100

    next 15

--- contracts/config.ts
+++ contracts/config.ts
@@ -1,4 +1,4 @@
 import MafiaABI from './MafiaPortal.json';
 
-export const MAFIA_CONTRACT_ADDRESS = "0x32D3612009c2d30C71C19d2548822E1EECb8D165";
+export const MAFIA_CONTRACT_ADDRESS = "0x3C1Bd1923F8318247e2B60E41B0F280391c4e1E1";
 export const MAFIA_ABI = MafiaABI.abi;
