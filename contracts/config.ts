576298a0e883ff82eebc2bdf18a49c0f203cba5d
Maksim <mpiatlitski@edu.cdv.pl>
2026-01-15 04:37:45 +0100

    fix: rename votes to killVotes to avoid shadowing, update contract address

--- contracts/config.ts
+++ contracts/config.ts
@@ -1,4 +1,4 @@
 import MafiaABI from './MafiaPortal.json';
 
-export const MAFIA_CONTRACT_ADDRESS = "0xe3905bcc708587cc7b624c21b65d26d599b69a0a";
+export const MAFIA_CONTRACT_ADDRESS = "0x88bb15623d6508e68528d3a0f7bd5f5fd9ea960a";
 export const MAFIA_ABI = MafiaABI.abi;
